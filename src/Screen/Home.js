7d9b1cff2d087f1976a8857699d505e017cd9d8f
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-11-06 01:19:18 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import './Home.css';
 
+import { SearchOutlined } from '@ant-design/icons';
+import { Button, Flex, Tooltip } from 'antd';
+
+import Calculator from '../Icon/keys.png';
+import ScientificCalculator from '../Icon/scientific-calculator.png';
+import Calendar from '../Icon/calendar.png';
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Button className='HomeItemButton'>
+        <img src={Calculator}/>
+        <h2>Calculator</h2>
+        <div>
+          asdfgh
+        </div>
+      </Button>
+      <Button className='HomeItemButton'>calendar</Button>
+    </div>
   )
 }
