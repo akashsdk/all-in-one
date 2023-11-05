6d66cb97b7fdbca3681f57922657fc77b7f81334
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-11-06 03:10:28 +0600

    ok

--- src/Cart/HomeItem.js
+++ src/Cart/HomeItem.js
@@ -1,10 +1,12 @@
 import React from "react";
+import './HomeItem.css';
 
-export default function HomeItem() {
+export default function HomeItem(props) {
   return (
     <div className="HomeItemBody">
-      <img className="HomeItemImage" src={""} alt={""} />
-      <h2 className="HomeItemText-1">Maps</h2>
+      <img className="HomeItemImage" src={props.Img} alt={""} />
+      <h2 className="HomeItemText-1">{props.FastText}</h2>
+      <h2 className="HomeItemText-2">{props.ScendText}</h2>
     </div>
   );
 }
--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import HomeItem from "../Cart/HomeItem";
 
 import { SearchOutlined } from "@ant-design/icons";
 import { Button, Flex, Tooltip } from "antd";
@@ -15,104 +16,60 @@ import Timers from "../Icon/sand-clock.png";
 import Notepad from "../Icon/notes.png";
 import voiceRecord from "../Icon/voice.png";
 import internetSpeed from "../Icon/medium.png";
-import Maps from '../Icon/map.png';
+import Maps from "../Icon/map.png";
 
 export default function Home() {
   return (
-    <div>
-      <div style={{ display: "flex" }}>
+    <div style={{ display: "flex" }}>
+      <div>
         {/* Calculator */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img className="HomeItemImage" src={Calculator} alt={""} />
-            <h2 className="HomeItemText-1">Calculator</h2>
-            <h2 className="HomeItemText-2"></h2>
-          </div>
+          <HomeItem Img={Calculator} FastText="Calculator" />
         </Link>
         {/* ScientificCalculator */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img
-              className="HomeItemImage"
-              src={ScientificCalculator}
-              alt={""}
-            />
-            <h2 className="HomeItemText-1">Scientific</h2>
-            <h2 className="HomeItemText-2">Calculator</h2>
-          </div>
+          <HomeItem
+            Img={ScientificCalculator}
+            FastText="Scientific"
+            ScendText="Calculator"
+          />
         </Link>
         {/* GPA Calculator */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img className="HomeItemImage" src={GPACalculator} alt={""} />
-            <h2 className="HomeItemText-1">GPA</h2>
-            <h2 className="HomeItemText-2">Calculator</h2>
-          </div>
+          <HomeItem Img={GPACalculator} FastText="GPA" ScendText="Calculator" />
         </Link>
         {/* Calendar */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img className="HomeItemImage" src={Calendar} alt={""} />
-            <h2 className="HomeItemText-1">Calendar</h2>
-          </div>
+          <HomeItem Img={Calendar} FastText="Calendar" ScendText="" />
         </Link>
         {/* rubik's cube */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img className="HomeItemImage" src={rubikCube} alt={""} />
-            <h2 className="HomeItemText-1">Rubik's</h2>
-            <h2 className="HomeItemText-2">Cube</h2>
-          </div>
+          <HomeItem Img={rubikCube} FastText="Rubik's" ScendText="Cube" />
         </Link>
-      </div>
-      <div style={{ display: "flex" }}>
         {/* Stopwatch */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img className="HomeItemImage" src={Stopwatch} alt={""} />
-            <h2 className="HomeItemText-1">Stopwatch</h2>
-          </div>
+          <HomeItem Img={Stopwatch} FastText="Stopwatch" ScendText="" />
         </Link>
         {/* Timers */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img className="HomeItemImage" src={Timers} alt={""} />
-            <h2 className="HomeItemText-1">Timer's</h2>
-          </div>
+          <HomeItem Img={Timers} FastText="Timer's" ScendText="" />
         </Link>
         {/* Note pad */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img className="HomeItemImage" src={Notepad} alt={""} />
-            <h2 className="HomeItemText-1">Notepad</h2>
-          </div>
+          <HomeItem Img={Notepad} FastText="Notepad" ScendText="" />
         </Link>
         {/* voice Record */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img className="HomeItemImage" src={voiceRecord} alt={""} />
-            <h2 className="HomeItemText-1">Voice</h2>
-            <h2 className="HomeItemText-2">Record</h2>
-          </div>
+          <HomeItem Img={voiceRecord} FastText="Voice" ScendText="Record" />
         </Link>
         {/* Internet Speed */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img className="HomeItemImage" src={internetSpeed} alt={""} />
-            <h2 className="HomeItemText-1">Internet</h2>
-            <h2 className="HomeItemText-2">Speed</h2>
-          </div>
+          <HomeItem Img={internetSpeed} FastText="Internet" ScendText="Speed" />
         </Link>
-      </div>
-      <div style={{ display: "flex" }}>
         {/* Maps */}
         <Link style={{ textDecoration: "none" }} to="/">
-          <div className="HomeItemBody">
-            <img className="HomeItemImage" src={Maps} alt={""} />
-            <h2 className="HomeItemText-1">Maps</h2>
-          </div>
+          <HomeItem Img={Maps} FastText="Maps" ScendText="" />
         </Link>
-        
       </div>
     </div>
   );
