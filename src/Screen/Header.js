bae86a5ddc921eb4365456cfff891b16b496b933
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-11-06 00:55:54 +0600

    ok

--- src/Screen/Header.js
+++ src/Screen/Header.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="HeaderBody">
+      <div  style={{ flex:'1' }}>
+        <Link to="/">
+          <h1 style={{ color: "white" }}>App Name</h1>
+        </Link>
+      </div>
+    </div>
+  );
 }
