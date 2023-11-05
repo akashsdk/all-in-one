c0f0e04a19665be8cbc8956502061298d5b3507c
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-11-06 02:04:36 +0600

    0

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -8,20 +8,51 @@ import { Link } from "react-router-dom";
 import Calculator from "../Icon/keys.png";
 import ScientificCalculator from "../Icon/scientific-calculator.png";
 import Calendar from "../Icon/calendar.png";
+import rubikCube from '../Icon/rubik.png';
+import GPACalculator from '../Icon/gpa.png';
 
 export default function Home() {
   return (
     <div>
       {/* Calculator */}
-      <Link to="/">
+      <Link style={{textDecoration:'none'}} to="/" >
         <div className="HomeItemBody">
           <img className="HomeItemImage" src={Calculator} alt={""} />
           <h2 className="HomeItemText-1">Calculator</h2>
           <h2 className="HomeItemText-2"></h2>
         </div>
       </Link>
+      {/* ScientificCalculator */}
+      <Link style={{textDecoration:'none'}} to="/" >
+        <div className="HomeItemBody">
+          <img className="HomeItemImage" src={ScientificCalculator} alt={""} />
+          <h2 className="HomeItemText-1">Scientific</h2>
+          <h2 className="HomeItemText-2">Calculator</h2>
+        </div>
+      </Link>
       {/* Calendar */}
-      <div className="HomeItemBody">calendar</div>
+      <Link style={{textDecoration:'none'}} to="/" >
+        <div className="HomeItemBody">
+          <img className="HomeItemImage" src={Calendar} alt={""} />
+          <h2 className="HomeItemText-1">Calendar</h2>
+        </div>
+      </Link>
+      {/* rubik's cube */}
+      <Link style={{textDecoration:'none'}} to="/" >
+        <div className="HomeItemBody">
+          <img className="HomeItemImage" src={rubikCube} alt={""} />
+          <h2 className="HomeItemText-1">Rubik's</h2>
+          <h2 className="HomeItemText-2">Cube</h2>
+        </div>
+      </Link>
+      {/* GPA Calculator */}
+      <Link style={{textDecoration:'none'}} to="/" >
+        <div className="HomeItemBody">
+          <img className="HomeItemImage" src={GPACalculator} alt={""} />
+          <h2 className="HomeItemText-1">GPA</h2>
+          <h2 className="HomeItemText-2">Calculator</h2>
+        </div>
+      </Link>
     </div>
   );
 }
