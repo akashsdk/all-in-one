01115559b0c3c934fd7ab58f0350f7b07d8961ed
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-11-06 00:32:51 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+import ErrorPage from './Screen/ErrorPage';
+import Home from "./Screen/Home";
+import Header from "./Screen/Header";
+import Footer from "./Screen/Footer";
+import Contact from "./Screen/Contact";
+
 
 function App() {
   return (
     <div className="App">
-      Akash
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="*" element={<ErrorPage />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Screen/ErrorPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, Result } from "antd";
+import { Link } from "react-router-dom";
+
+export default function ErrorPage() {
+  return (
+    <div>
+        <div style={{height:'60px'}}/>
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={<Button type="primary">
+            <Link to="/">Back Home</Link>
+        </Button>}
+      />
+      <div style={{height:'80px'}}/>
+    </div>
+  );
+}
