1e5599607f7e1bd372648ae87420b738a4be40d3
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-11-06 01:31:21 +0600

    ok

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
-import { SearchOutlined } from '@ant-design/icons';
-import { Button, Flex, Tooltip } from 'antd';
+import { SearchOutlined } from "@ant-design/icons";
+import { Button, Flex, Tooltip } from "antd";
+import { Link } from "react-router-dom";
 
-import Calculator from '../Icon/keys.png';
-import ScientificCalculator from '../Icon/scientific-calculator.png';
-import Calendar from '../Icon/calendar.png';
+import Calculator from "../Icon/keys.png";
+import ScientificCalculator from "../Icon/scientific-calculator.png";
+import Calendar from "../Icon/calendar.png";
 
 export default function Home() {
   return (
     <div>
-      <Button className='HomeItemButton'>
-        <img src={Calculator}/>
-        <h2>Calculator</h2>
-        <div>
-          asdfgh
+      {/* Calculator */}
+      <Link to="/">
+        <div className="HomeItemBody">
+          <img className="HomeItemImage" src={Calculator} alt={""} />
+          <h2 className="HomeItemText-1">Calculator</h2>
+          <h2 className="HomeItemText-2"></h2>
         </div>
-      </Button>
-      <Button className='HomeItemButton'>calendar</Button>
+      </Link>
+      {/* Calendar */}
+      <div className="HomeItemBody">calendar</div>
     </div>
-  )
+  );
 }
