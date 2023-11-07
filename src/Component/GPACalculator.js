b5ebd2dff3ebe5c39f7873487d36b95e3b341a73
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-11-07 17:36:50 +0600

    ok

--- src/Component/GPACalculator.js
+++ src/Component/GPACalculator.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Component.css";
 import ComponentCart from "../Cart/ComponentCart";
 
-import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Space, Select } from "antd";
-const onFinish = (values) => {
-  console.log("Received values of form:", values);
-};
-
-const handleChange = (value) => {
-  console.log(`selected ${value}`);
-};
+import { MinusCircleOutlined } from "@ant-design/icons";
+import { Button } from "antd";
 
 export default function GPACalculator() {
+  const [courses, setCourses] = useState([
+    { name: "", grade: "", credits: "" },
+  ]);
+  const [gpa, setGpa] = useState(0);
+
+  const handleCourseChange = (e, index) => {
+    const { name, value } = e.target;
+    const updatedCourses = [...courses];
+    updatedCourses[index][name] = value;
+    setCourses(updatedCourses);
+  };
+
+  const addCourse = () => {
+    setCourses([...courses, { name: "", grade: "", credits: "" }]);
+  };
+
+  const removeCourse = (index) => {
+    const updatedCourses = [...courses];
+    updatedCourses.splice(index, 1);
+    setCourses(updatedCourses);
+  };
+
+  const calculateGpa = () => {
+    let totalPoints = 0;
+    let totalCredits = 0;
+
+    for (const course of courses) {
+      const credit = parseFloat(course.credits);
+      totalCredits += credit;
+      totalPoints += parseFloat(course.grade) * credit;
+    }
+
+    const gpaValue = totalPoints / totalCredits;
+    setGpa(gpaValue);
+  };
+
+  const clean = () => {
+    
+  };
   return (
     <div>
       <ComponentCart
@@ -20,127 +52,56 @@ export default function GPACalculator() {
         bodyText="Perform bitwise and bit shift operations."
       />
       <div className="ComponentBody">
-        <div className="gpaBody">
-          <div style={{ height: "30px" }} />
-          <Form
-            name="dynamic_form_nest_item"
-            onFinish={onFinish}
-            style={{
-              maxWidth: 600,
-            }}
-            autoComplete="off"
-          >
-            <Form.List name="users">
-              {(fields, { add, remove }) => (
-                <>
-                  {fields.map(({ key, name, ...restField }) => (
-                    <Space
-                      key={key}
-                      style={{
-                        display: "flex",
-                        marginBottom: 8,
-                      }}
-                      align="baseline"
-                    >
-                      <Form.Item
-                        {...restField}
-                        name={[name, "Course"]}
-                        rules={[
-                          {
-                            required: true,
-                            message: "Missing Course name",
-                          },
-                        ]}
-                      >
-                        <Input placeholder="Course" />
-                      </Form.Item>
-                      <Form.Item
-                        {...restField}
-                        name={[name, "value"]}
-                        rules={[
-                          {
-                            required: true,
-                            message: "Missing Grad",
-                          },
-                        ]}
-                      >
-                        <Select
-                          // defaultValue='A+'
-                          style={{
-                            width: 120,
-                          }}
-                          onChange={handleChange}
-                          options={[
-                            {
-                              value: "4.00",
-                              label: "A+",
-                            },
-                            {
-                              value: "	3.75",
-                              label: "A",
-                            },
-                            {
-                              value: "	3.50",
-                              label: "A-",
-                            },
-                            {
-                              value: "3.25",
-                              label: "B+",
-                            },
-                            {
-                              value: "3.00",
-                              label: "B",
-                            },
-                            {
-                              value: "2.75",
-                              label: "B-",
-                            },
-                            {
-                              value: "2.50",
-                              label: "C+",
-                            },
-                            {
-                              value: "2.25",
-                              label: "C",
-                            },
-                            {
-                              value: "2.00",
-                              label: "D",
-                            },
-                            {
-                              value: "0",
-                              label: "F",
-                            },
-                          ]}
-                        />
-                      </Form.Item>
-                      <MinusCircleOutlined onClick={() => remove(name)} />
-                    </Space>
-                  ))}
-                  <Form.Item>
-                    <Button
-                      type="dashed"
-                      onClick={() => add()}
-                      block
-                      icon={<PlusOutlined />}
-                      style={{
-                        maxWidth: 400,
-                      }}
-                    >
-                      Add Course
-                    </Button>
-                  </Form.Item>
-                </>
-              )}
-            </Form.List>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Gat GPA
-              </Button>
-            </Form.Item>
-          </Form>
-          <h2 className="gpaH2Text">Your GPA is: 4.00</h2>
+        <div style={{ height: "20px" }} />
+        <div>
+          <h1 style={{ color: "rgb(245, 4, 112" }}>Result: {gpa.toFixed(2)}</h1>
+          {courses.map((course, index) => (
+            <div key={index} style={{ marginTop: "10px" }}>
+              <input
+                className="GPA-Input"
+                type="text"
+                placeholder="Course Name"
+                name="name"
+                value={course.name}
+                onChange={(e) => handleCourseChange(e, index)}
+              />
+              <input
+                className="GPA-Input"
+                type="number"
+                placeholder="Grade (0-4)"
+                name="grade"
+                value={course.grade}
+                onChange={(e) => handleCourseChange(e, index)}
+              />
+              <input
+                className="GPA-Input"
+                type="number"
+                placeholder="Credits"
+                name="credits"
+                value={course.credits}
+                onChange={(e) => handleCourseChange(e, index)}
+              />
+              <Button
+                className="GPA-Button"
+                shape="circle"
+                onClick={() => removeCourse(index)}
+                icon={<MinusCircleOutlined />}
+              />
+            </div>
+          ))}
+
+          <div style={{marginTop:'20px'}}>
+            <Button  onClick={addCourse}>Add Course</Button>
+            <Button style={{marginLeft:'10px'}} type="primary" onClick={calculateGpa}>
+              Calculate GPA
+            </Button>
+            <Button style={{marginLeft:'10px'}} onClick={clean} type="dashed" danger>
+            Clean
+            </Button>
+          </div>
+          
         </div>
+        <div style={{ height: "30px" }} />
       </div>
     </div>
   );
