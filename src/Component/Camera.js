2a98d3dd367e18f25c442d109e92985ab01ef1e2
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-11-08 01:14:12 +0600

    ok

--- src/Component/Camera.js
+++ src/Component/Camera.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import './Component.css';
+import ComponentCart from "../Cart/ComponentCart";
 
 export default function Camera() {
   return (
-    <div>Camera</div>
+    <div>
+      <ComponentCart
+        mainText="Camera"
+        bodyText="Camera"
+      />
+      <div className="ComponentBody">
+        dfg
+      </div>
+    </div>
   )
 }
--- src/Component/GPACalculator.js
+++ src/Component/GPACalculator.js
@@ -8,7 +8,10 @@ import { Button } from "antd";
 export default function GPACalculator() {
   return (
     <div>
-      <ComponentCart mainText="GPA Calculator" bodyText="GPA Calculator." />
+      <ComponentCart
+        mainText="GPA Calculator"
+        bodyText="The calculator can be used to determine the minimum GPA required in future courses to raise GPA to a desired level or maintain the GPA above a certain level."
+      />
 
       <div className="ComponentBody">GPACalculator</div>
     </div>
