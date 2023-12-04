7f1117fc2c16425d35f25aeb9cd3a2b25b99b133
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-05 01:48:36 +0600

    ok

--- src/Component/Stopwatch.js
+++ src/Component/Stopwatch.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./Component.css";
 import ComponentCart from "../Cart/ComponentCart";
 
 export default function Stopwatch() {
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000); // Update every 1 second (1000 milliseconds)
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setTimer(0);
+    setIsRunning(false);
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  };
+
   return (
     <div>
       <ComponentCart mainText="Stopwatch" bodyText="Stopwatch" />
       <div className="ComponentBody">
-        
+      <div className="stopwatch">
+      <h1>Stopwatch</h1>
+      <div className="timer">{formatTime(timer)}</div>
+      <div className="controls">
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
       </div>
     </div>
   );
