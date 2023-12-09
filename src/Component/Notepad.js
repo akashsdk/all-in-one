c5c7112da03af8ab83507dd972f907e6444e6e63
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-09 23:38:16 +0600

    Update Notepad.js

--- src/Component/Notepad.js
+++ src/Component/Notepad.js
@@ -1,11 +1,9 @@
-import React, {useState} from 'react'
-import './Component.css';
+import React, { useState } from "react";
+import "./Component.css";
 import ComponentCart from "../Cart/ComponentCart";
 
-
 export default function Notepad() {
-
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
   // Function to handle text change
   const handleTextChange = (event) => {
@@ -14,19 +12,19 @@ export default function Notepad() {
 
   // Function to handle download button click
   const handleDownload = () => {
-    if (text.trim() === '') {
-      alert('The Notepad is empty. Please add some text before downloading.');
+    if (text.trim() === "") {
+      alert("The Notepad is empty. Please add some text before downloading.");
       return;
     }
 
     // Create a blob with the text content
-    const blob = new Blob([text], { type: 'text/plain' });
+    const blob = new Blob([text], { type: "text/plain" });
     const url = URL.createObjectURL(blob);
 
     // Create a temporary <a> element to trigger the download
-    const a = document.createElement('a');
+    const a = document.createElement("a");
     a.href = url;
-    a.download = 'notepad_text.txt';
+    a.download = "notepad_text.txt";
     document.body.appendChild(a);
     a.click();
 
@@ -37,23 +35,25 @@ export default function Notepad() {
 
   // Function to handle clean button click
   const handleClean = () => {
-    setText('');
+    setText("");
   };
   return (
     <div>
-      <ComponentCart
-        mainText="Notepad"
-        bodyText="Notepad..."
-      />
+      <ComponentCart mainText="Notepad" bodyText="Notepad..." />
       <div className="ComponentBody">
-      <div>
-      <h1>Notepad</h1>
-      <textarea value={text} onChange={handleTextChange} rows={10} cols={50} />
-      <br />
-      <button onClick={handleDownload}>Download</button>
-      <button onClick={handleClean}>Clean</button>
-    </div>
+        <div>
+          <h1>Notepad</h1>
+          <textarea
+            value={text}
+            onChange={handleTextChange}
+            rows={10}
+            cols={50}
+          />
+          <br />
+          <button onClick={handleDownload}>Download</button>
+          <button onClick={handleClean}>Clean</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
