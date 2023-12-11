c8f7fa8cc0f82aae1653f06d9994ff26b65086f2
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-11 17:13:54 +0600

    Update RubikCube.js

--- src/Component/RubikCube.js
+++ src/Component/RubikCube.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import './Component.css';
+import ComponentCart from "../Cart/ComponentCart";
 
 export default function RubikCube() {
   return (
-    <div>RubikCube</div>
+    <div>
+      <ComponentCart
+        mainText="RubikCube"
+        bodyText="RubikCube..."
+      />
+      <div className="ComponentBody">
+        dfg
+      </div>
+    </div>
   )
 }
