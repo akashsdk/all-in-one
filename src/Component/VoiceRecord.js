8a0ba11a1c81be33d7324ec8db6b8444b6cc3e73
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-13 01:19:31 +0600

    Update VoiceRecord.js

--- src/Component/VoiceRecord.js
+++ src/Component/VoiceRecord.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
 import './Component.css';
 import ComponentCart from "../Cart/ComponentCart";
 
 export default function VoiceRecord() {
+  const [recording, setRecording] = useState(false);
+  const [audioBlob, setAudioBlob] = useState(null);
+
+  const mediaRecorder = useRef(null);
+  const audioRef = useRef(null);
+
+  const startRecording = () => {
+    const stream = navigator.mediaDevices.getUserMedia({ audio: true });
+
+    stream.then((stream) => {
+      mediaRecorder.current = new MediaRecorder(stream);
+      const chunks = [];
+
+      mediaRecorder.current.ondataavailable = (e) => {
+        chunks.push(e.data);
+      };
+
+      mediaRecorder.current.onstop = () => {
+        const blob = new Blob(chunks, { type: 'audio/wav' });
+        setAudioBlob(blob);
+      };
+
+      mediaRecorder.current.start();
+      setRecording(true);
+    }).catch((err) => {
+      console.error('Error accessing microphone:', err);
+    });
+  };
+
+  const stopRecording = () => {
+    if (mediaRecorder.current && mediaRecorder.current.state !== 'inactive') {
+      mediaRecorder.current.stop();
+      setRecording(false);
+    }
+  };
+
+  const downloadAudio = () => {
+    if (audioBlob) {
+      const url = URL.createObjectURL(audioBlob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'recorded_audio.wav';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
   return (
     <div>
       <ComponentCart
@@ -10,7 +57,20 @@ export default function VoiceRecord() {
         bodyText="Voice Record...."
       />
       <div className="ComponentBody">
-        dfg
+      <div>
+      <h2>Voice Recorder</h2>
+      <audio ref={audioRef} controls />
+      <div>
+        {recording ? (
+          <button onClick={stopRecording}>Stop Recording</button>
+        ) : (
+          <button onClick={startRecording}>Start Recording</button>
+        )}
+        {audioBlob && (
+          <button onClick={downloadAudio}>Download Audio</button>
+        )}
+      </div>
+    </div>
       </div>
     </div>
   )
