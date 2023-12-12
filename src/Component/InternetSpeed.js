82bf294c8a2a3cd1c51a8848ee97d5316d34f525
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-13 01:45:39 +0600

    ok

--- src/Component/InternetSpeed.js
+++ src/Component/InternetSpeed.js
@@ -1,30 +1,33 @@
-import React from 'react'
-import './Component.css';
+import React from "react";
+import "./Component.css";
 import ComponentCart from "../Cart/ComponentCart";
 
-import fastLOgo from '../Icon/Screenshot 2023-12-13 at 1.24.46 AM.png';
-
+import fastLOgo from "../Icon/Screenshot 2023-12-13 at 1.24.46 AM.png";
 
 export default function InternetSpeed() {
+  const goToFacebook = () => {
+    window.open('https://www.facebook.com', '_blank');  // Replace with the desired Facebook URL
+  };
   return (
     <div>
-      <ComponentCart
-        mainText="Internet Speed"
-        bodyText="InternetSpeed..."
-      />
+      <ComponentCart mainText="Internet Speed" bodyText="InternetSpeed..." />
       <div className="ComponentBody">
-      <a
-            style={{ textDecoration: "none" }}
-            href="https://fast.com/"
-            target="_blank"
-          >
-            aksh
-          </a>
-          
-          <div className='InternetSpeedDiv'>
-          <img height={170} width={200} src={fastLOgo} alt=''/>
-          </div>
+        <a
+          style={{ textDecoration: "none" }}
+          href="https://fast.com/"
+          target="_blank"
+        >
+          <div className="InternetSpeedDiv">
+          <img height={180} width={200} src={fastLOgo} alt="" />
+        </div>
+        </a>
+        {/* <button onClick={goToFacebook}>
+        <div className="InternetSpeedDiv">
+          <img height={10} width={200} src={fastLOgo} alt="" />
+        </div>
+        </button> */}
+        
       </div>
     </div>
-  )
+  );
 }
