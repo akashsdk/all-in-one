71c22d64a2216b87327ff729f587ae62b12d08ae
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-13 01:38:25 +0600

    ok

--- src/Component/InternetSpeed.js
+++ src/Component/InternetSpeed.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './Component.css';
 import ComponentCart from "../Cart/ComponentCart";
 
+import fastLOgo from '../Icon/Screenshot 2023-12-13 at 1.24.46 AM.png';
+
 
 export default function InternetSpeed() {
   return (
@@ -11,7 +13,17 @@ export default function InternetSpeed() {
         bodyText="InternetSpeed..."
       />
       <div className="ComponentBody">
-        
+      <a
+            style={{ textDecoration: "none" }}
+            href="https://fast.com/"
+            target="_blank"
+          >
+            aksh
+          </a>
+          
+          <div className='InternetSpeedDiv'>
+          <img height={170} width={200} src={fastLOgo} alt=''/>
+          </div>
       </div>
     </div>
   )
