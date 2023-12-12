6f63244c7393340e104213220977bd596c0ac735
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-13 01:16:08 +0600

    Update VoiceRecord.js

--- src/Component/VoiceRecord.js
+++ src/Component/VoiceRecord.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import './Component.css';
+import ComponentCart from "../Cart/ComponentCart";
 
 export default function VoiceRecord() {
   return (
-    <div>VoiceRecord</div>
+    <div>
+      <ComponentCart
+        mainText="Voice Record"
+        bodyText="Voice Record...."
+      />
+      <div className="ComponentBody">
+        dfg
+      </div>
+    </div>
   )
 }
