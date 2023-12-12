306a2a3521b62b7d85f11bc3e21d6d7ce094ed7e
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2023-12-13 01:56:28 +0600

    ok

--- src/Component/InternetSpeed.js
+++ src/Component/InternetSpeed.js
@@ -3,15 +3,28 @@ import "./Component.css";
 import ComponentCart from "../Cart/ComponentCart";
 
 import fastLOgo from "../Icon/Screenshot 2023-12-13 at 1.24.46 AM.png";
+import googleLogo from '../Icon/google-fiber-logo.png';
+import intLogo from '../Icon/download-speedtest net.png';
 
 export default function InternetSpeed() {
-  const goToFacebook = () => {
-    window.open('https://www.facebook.com', '_blank');  // Replace with the desired Facebook URL
-  };
   return (
     <div>
       <ComponentCart mainText="Internet Speed" bodyText="InternetSpeed..." />
       <div className="ComponentBody">
+        <div>
+        <a
+          style={{ textDecoration: "none" }}
+          href="https://www.speedtest.net/"
+          target="_blank"
+        >
+          <div className="InternetSpeedDiv">
+          <img height={180} width={200} src={intLogo} alt="" />
+        </div>
+        </a>
+        
+
+
+        <div style={{height:'20px'}} />
         <a
           style={{ textDecoration: "none" }}
           href="https://fast.com/"
@@ -21,11 +34,18 @@ export default function InternetSpeed() {
           <img height={180} width={200} src={fastLOgo} alt="" />
         </div>
         </a>
-        {/* <button onClick={goToFacebook}>
-        <div className="InternetSpeedDiv">
-          <img height={10} width={200} src={fastLOgo} alt="" />
+        
+        <div style={{height:'20px'}} />
+        <a
+          style={{ textDecoration: "none" }}
+          href="https://fiber.google.com/speedtest/"
+          target="_blank"
+        >
+          <div className="InternetSpeedDiv">
+          <img height={180} width={200} src={googleLogo} alt="" />
         </div>
-        </button> */}
+        </a>
+        </div>
         
       </div>
     </div>
